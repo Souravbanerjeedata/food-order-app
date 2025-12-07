915258be8dd96ea4349e48690b8136d518e1bf1a
Sourav <sourav.banerjeejobs@gmail.com>
2025-12-07 13:13:08 +0530

    header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import logoImg from "../assets/logo.jpg";
+const Header = () => {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="website logo" />
+        <h1>Your Order</h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
